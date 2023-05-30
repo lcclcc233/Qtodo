51148d02ac952662440760fd003534f2a0370b3d
Jia <108002695+Flashbac09@users.noreply.github.com>
2023-05-30 14:45:06 +0800

    use qmake to configure, get icon done, add tasklist ui frame
    I had trouble in using cmakelist.txt to add resource file. Therefore, i use qmake and add the icon for widgets.

--- dialog.h
+++ dialog.h
@@ -18,6 +18,10 @@ public:
 private slots:
     void on_buttonBox_accepted();
 
+    void on_wholeday_checkBox_stateChanged(int arg1);
+
+    void on_dateTimeEdit_dateTimeChanged(const QDateTime &dateTime);
+
 private:
     Ui::Dialog *ui;
 };
